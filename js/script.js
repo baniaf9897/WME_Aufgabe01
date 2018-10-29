296c9ffab2b646404d69287d1e4c3cf1beda80f3
Adrian Jentzsch <jentzsch.adrian@web.de>
2018-10-29 21:20:25 +0100

    Sortieren der Tabelle

--- js/script.js
+++ js/script.js
@@ -105,6 +105,65 @@ function ausblendenInternet() {
         stateInternet = 0;
     }
 }
+//Sortierfunktion nach Vorbild der W3School
+function sortUp(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, switchcount = 0;
+
+    table = document.getElementById("table");
+    switching = true;
+    while (switching){
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++){
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch= true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount ++;
+        } else {
+            if (switchcount == 0) {
+                switching = true;
+            }
+        }
+    }
+}
+
+function sortDown(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, switchcount = 0;
+
+    table = document.getElementById("table");
+    switching = true;
+    while (switching){
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++){
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount ++;
+        } else {
+            if (switchcount == 0) {
+                switching = true;
+            }
+        }
+    }
+}
+
     /**************** responsive navbar  ******************/
 
     
